db78c22c56e690b4b97e8abf3669a3716455eef2
Eliza <fshank4@comcast.net>
2018-10-19 15:09:14 -0400

    changed page title

--- iCarShopWebApp/inventory.php
+++ iCarShopWebApp/inventory.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Table V01</title>
+	<title>View Inventory | iCarShop</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
