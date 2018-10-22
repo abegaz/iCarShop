dd4bf7ac4b770483050a2b74dce039aad8733fd8
Will Sexton <34824200+codinlikewilly@users.noreply.github.com>
2018-10-22 14:53:42 -0400

    Update inventory.php

--- iCarShopWebApp/inventory.php
+++ iCarShopWebApp/inventory.php
@@ -20,8 +20,45 @@
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
 </head>
 <body>
+
+<div class="topnav">
+  <a class="HeroPage.html" href="#home">Home</a>
+  <a href="CarDataEntryView.html">ADMIN</a>
+  
+</div>
 	
 	<div class="limiter">
 		<div class="container-table100">
@@ -74,6 +111,10 @@
 			</div>
 		</div>
 	</div>
+	
+	
+  
+</div>
 
 
 	
