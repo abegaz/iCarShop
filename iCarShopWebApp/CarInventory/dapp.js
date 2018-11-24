e6785179c6dedb76dd9e1c489560c54bc5d3e8f2
cbfost8944 <42586026+cbfost8944@users.noreply.github.com>
2018-11-23 19:11:21 -0500

    detailing
    blah blah

--- iCarShopWebApp/CarInventory/dapp.js
+++ iCarShopWebApp/CarInventory/dapp.js
@@ -17,17 +17,6 @@ function renderCafe(doc){
     let vin = document.createElement('span');
     let stats = document.createElement('div');
     document.getElementById('CarTitle').innerHTML = doc.data().make + " " + doc.data().model;
- 
-
-    /*function show_image(image_source, width, height,alt) {
-        let image = document.createElement('img');
-        image.src = image_source;
-        image.width = width;
-        image.height = height;
-        image.alt = alt;
-        return image;
-    }*/
-
 
     li.setAttribute('data-id', doc.id);
     make.textContent = "Make: " + doc.data().make;
@@ -56,18 +45,44 @@ function renderCafe(doc){
     li.appendChild(stats);
 
     cafeList.appendChild(li);
-// deleting data
-/*cross.addEventListener('click', (e) =>{
-    //stops continous propagation of the event (optional but prevents errors)
-    e.stopPropagation();
-    //grabs attribute tied to UI element (which is also connected to Firebase ID)
-    let id = e.target.parentElement.getAttribute('data-id');
-    //queries firestore and deletes that id
-    db.collection('vehicles').doc(id).delete();
 
-})*/
 }
+
+
+
+
+
 const urlParams = new URLSearchParams(window.location.search);
+
+window.onload = db.collection('vehicles').where('vin','==', urlParams.get('vin')).get().then((snapshot)=> {
+    snapshot.docs.forEach(doc => {
+        var data = doc.data();
+        console.table(data);
+
+        var URL_1 = doc.data().images.First_Image;
+        console.log('Image Data Follows: ' + URL_1);
+
+        //  document.getElementById("1st_URL").innerHTML = URL_1;
+
+        var URL_2 = doc.data().images.Second_Image;
+        console.log('Image Data Follows: ' + URL_2);
+
+        //  document.getElementById("2nd_URL").innerHTML = URL_2;
+
+        var URL_3 = doc.data().images.Third_Image;
+        console.log('Image Data Follows: ' + URL_3);
+
+        //  document.getElementById("3rd_URL").innerHTML = URL_3;
+        
+        document.getElementById('Image_1').src = URL_1;
+        document.getElementById('Image_2').src = URL_2;
+        document.getElementById('Image_3').src = URL_3;
+
+
+
+        })
+
+        })
 
 
 // getting data
@@ -76,255 +91,11 @@ const urlParams = new URLSearchParams(window.location.search);
 // ex: where('city', '==' , 'manchester'), is case sensitive
 window.onload = db.collection('vehicles').where('vin','==', urlParams.get('vin')).get().then((snapshot)=> {
         snapshot.docs.forEach(doc => {
-        renderCafe(doc);
+         renderCafe(doc);
+
+
 
             })
 
             })
 
-/*function load() 
-{
-    document.getElementById("cafe-list").innerHTML = "";
-    if (form.make.value != '' ||
-    form.model.value != '' ||
-    form.year.value != '' ||
-    form.body.value != '' ||
-    form.price.value != '' ||
-    form.mileage.value != '' ||
-    form.interior_color.value != '' ||
-    form.exterior_color.value != '' ||
-    form.used.value != '')
-    {
-        var searchArray = [];
-        if (form.make.value != '')
-        {
-            searchArray.push(['make',form.make.value]);
-        }
-        if (form.model.value != '')
-        {
-            searchArray.push(['model',form.model.value]);
-        }
-        if (form.year.value != '')
-        {
-            searchArray.push(['year',form.year.value]);
-        }
-        if (form.body.value != '')
-        {
-            searchArray.push(['body',form.body.value]);
-        }
-        if (form.price.value != '')
-        {
-            searchArray.push(['price',form.price.value]);
-        }
-        if (form.mileage.value != '')
-        {
-            searchArray.push(['mileage',form.mileage.value]);
-        }
-        if (form.interior_color.value != '')
-        {
-            searchArray.push(['interior_color',form.interior_color.value]);
-        }
-        if (form.exterior_color.value != '')
-        {
-            searchArray.push(['exterior_color',form.exterior_color.value]);
-        }
-        if (form.used.value != '')
-        {
-            searchArray.push(['used',form.used.value]);
-        }   
-
-        if (searchArray.length == 1)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 2)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 3)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).where(searchArray[2][0],'==', searchArray[2][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 4)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).where(searchArray[2][0],'==', searchArray[2][1]).where(searchArray[3][0],'==', searchArray[3][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 5)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).where(searchArray[2][0],'==', searchArray[2][1]).where(searchArray[3][0],'==', searchArray[3][1]).where(searchArray[4][0],'==', searchArray[4][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 6)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).where(searchArray[2][0],'==', searchArray[2][1]).where(searchArray[3][0],'==', searchArray[3][1]).where(searchArray[4][0],'==', searchArray[4][1]).where(searchArray[5][0],'==', searchArray[5][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 7)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).where(searchArray[2][0],'==', searchArray[2][1]).where(searchArray[3][0],'==', searchArray[3][1]).where(searchArray[4][0],'==', searchArray[4][1]).where(searchArray[5][0],'==', searchArray[5][1]).where(searchArray[6][0],'==', searchArray[6][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 8)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).where(searchArray[2][0],'==', searchArray[2][1]).where(searchArray[3][0],'==', searchArray[3][1]).where(searchArray[4][0],'==', searchArray[4][1]).where(searchArray[5][0],'==', searchArray[5][1]).where(searchArray[6][0],'==', searchArray[6][1]).where(searchArray[7][0],'==', searchArray[7][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-        if (searchArray.length == 9)
-        {
-            db.collection('vehicles').where(searchArray[0][0],'==', searchArray[0][1]).where(searchArray[1][0],'==', searchArray[1][1]).where(searchArray[2][0],'==', searchArray[2][1]).where(searchArray[3][0],'==', searchArray[3][1]).where(searchArray[4][0],'==', searchArray[4][1]).where(searchArray[5][0],'==', searchArray[5][1]).where(searchArray[6][0],'==', searchArray[6][1]).where(searchArray[7][0],'==', searchArray[7][1]).where(searchArray[8][0],'==', searchArray[8][1]).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-        }
-
-        
-
-       
-    }
-    else
-    {
-      db.collection('vehicles').get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-    })
-
-    })  
-    }
-}
-
-// loading data
-form.addEventListener('submit',(e) =>{
-    e.preventDefault();
-    load();
-
-})
-
-form2.addEventListener('submit',(e) =>{
-    e.preventDefault();
-    document.getElementById("cafe-list").innerHTML = "";
-    if (form2.content.value == "")
-    {
-        load();
-    }
-    else
-    {
-    db.collection('vehicles').where('vin','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('make','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('model','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('year','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('price','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('body','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('mileage','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('interior_color','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('exterior_color','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    db.collection('vehicles').where('used','==', form2.content.value).get().then((snapshot)=> {
-        snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-
-            })
-
-            })
-    }
-})*/
